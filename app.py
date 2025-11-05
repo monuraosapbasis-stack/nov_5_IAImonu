7a141a69e5c114e2abc17b9d3d20b08c105ac67a
monuraosapbasis-stack <monuraosapbasis@gmail.com>
2025-11-05 13:35:12 -0800

    'monufirtcommit'

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Hello, Streamlit!")
+st.write("Welcome to your first Streamlit app.")
+st.button("Click me!")
+st.write("You clicked the button!" if st.button("Click me!") else "Button not clicked yet.")
+st.write("This is a simple app to demonstrate Streamlit functionality.")
